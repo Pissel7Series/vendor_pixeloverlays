85a920d260da727689b5e4a97a778903a0454bf7
Pyrtle93 <xxpyrtlexx93@gmail.com>
2025-04-07 00:28:17 +0530

    lineage>infinity

--- config.mk
+++ config.mk
@@ -6,51 +6,51 @@ ifeq ($(WITH_GMS),true)
 WITH_PIXEL_OVERLAYS?=true
 ifeq ($(WITH_PIXEL_OVERLAYS),true)
 
-ifneq (,$(filter akita,$(LINEAGE_BUILD)))
+ifneq (,$(filter akita,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/akita/akita-vendor.mk
 endif
 
-ifneq (,$(filter bluejay,$(LINEAGE_BUILD)))
+ifneq (,$(filter bluejay,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/bluejay/bluejay-vendor.mk
 endif
 
-ifneq (,$(filter caiman,$(LINEAGE_BUILD)))
+ifneq (,$(filter caiman,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/caiman/caiman-vendor.mk
 endif
 
-ifneq (,$(filter cheetah,$(LINEAGE_BUILD)))
+ifneq (,$(filter cheetah,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/cheetah/cheetah-vendor.mk
 endif
 
-ifneq (,$(filter comet,$(LINEAGE_BUILD)))
+ifneq (,$(filter comet,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/comet/comet-vendor.mk
 endif
 
-ifneq (,$(filter husky,$(LINEAGE_BUILD)))
+ifneq (,$(filter husky,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/husky/husky-vendor.mk
 endif
 
-ifneq (,$(filter komodo,$(LINEAGE_BUILD)))
+ifneq (,$(filter komodo,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/komodo/komodo-vendor.mk
 endif
 
-ifneq (,$(filter oriole,$(LINEAGE_BUILD)))
+ifneq (,$(filter oriole,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/oriole/oriole-vendor.mk
 endif
 
-ifneq (,$(filter panther,$(LINEAGE_BUILD)))
+ifneq (,$(filter panther,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/panther/panther-vendor.mk
 endif
 
-ifneq (,$(filter raven,$(LINEAGE_BUILD)))
+ifneq (,$(filter raven,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/raven/raven-vendor.mk
 endif
 
-ifneq (,$(filter shiba,$(LINEAGE_BUILD)))
+ifneq (,$(filter shiba,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/shiba/shiba-vendor.mk
 endif
 
-ifneq (,$(filter tokay,$(LINEAGE_BUILD)))
+ifneq (,$(filter tokay,$(INFINITY_BUILD)))
 include vendor/pixeloverlays/tokay/tokay-vendor.mk
 endif
 
